71632dd8adb0195a5a61287e058c4e9fbcd20fc2
amuif <amudihayat@gmail.com>
2025-07-06 13:02:58 +0300

    mid day push

--- services/api.ts
+++ services/api.ts
@@ -1,15 +1,25 @@
-const url =
-  "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc";
-const options = {
-  method: "GET",
+export const TMDB_CONFIG = {
+  BASE_URL: "https://api.themoviedb.org/3",
+  API_KEY: process.env.EXPO_PUBLIC_API_KEY,
   headers: {
     accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNTM0ODE5MGYyNjRlM2JlNWYxMjVhMGM0ZTI5NGUwMyIsIm5iZiI6MTc1MTc0NzQ5Ny43Niwic3ViIjoiNjg2OThiYTkyYzRlZmFjYmMxNTNmNWRhIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.qK2EAt4tgx0YiDd1BAxeUrraAO-0UeEclwaDzP9c9O8",
+    Authorization: `Bearer ${process.env.EXPO_PUBLIC_API_KEY}`,
   },
 };
 
-fetch(url, options)
-  .then((res) => res.json())
-  .then((json) => console.log(json))
-  .catch((err) => console.error(err));
+const fetchMovies = async ({ query }: { query: string }) => {
+  const endpoint = query
+    ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+    : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+  const response = await fetch(endpoint, {
+    method: "GET",
+    headers: TMDB_CONFIG.headers,
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch movies");
+  }
+  const data = await response.json();
+  return data;
+};
